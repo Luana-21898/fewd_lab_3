3ab4adb127229379981f674920a17f2a15fe5e56
Luana-21898 <21898@student.dorset-college.ie>
2021-02-03 03:43:43 +0000

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Question 3
+function greet() {
+    console.log('Haydo!');
+}
+var salutation = greet();
+
+// Question 4
+function echo(sound) {
+    return sound;
+    
+}
+
+// Question 5
+function greet(name) {
+    return('Hello ' + name + '!')
+}
+
+// Question 
+
+var whereIs = function(name) {
+    return 'Where is ' + name + '?';
+    };
+    var x = whereIs('Jack');
