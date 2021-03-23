efb8701fdb548744c1f6df96e4a141ab0c826280
Luana-21898 <21898@student.dorset-college.ie>
2021-03-23 22:38:43 +0000

    commit

--- script.js
+++ script.js
@@ -4,20 +4,57 @@ function greet() {
 }
 var salutation = greet();
 
+
 // Question 4
 function echo(sound) {
     return sound;
-    
-}
+    }
 
-// Question 5
+
+    // Question 5
 function greet(name) {
-    return('Hello ' + name + '!')
+    return console.log('Hello ' + name + '!');
+}
+greet('Luana');
+
+
+// Question 8
+function shout(string) {
+    return console.log(string + string);
+}
+shout('Fire');
+
+
+// Question 11
+function doSomething(name) {
+    return name;
+}
+console.log(doSomething('Luana'));
+
+
+// Question 12
+function multiplyFive(number) {
+    return 5 * number;
+}
+console.log(multiplyFive(5));
+
+
+// Question 13
+function myFunction() {
+  alert ("Hello World!");
 }
 
-// Question 
 
-var whereIs = function(name) {
-    return 'Where is ' + name + '?';
-    };
-    var x = whereIs('Jack');
+// Question 14
+var isAnswerRight = true;
+isAnswerRight ? console.log('The answer is right!') : ('The answer is wrong!');
+
+
+// Question 15
+function tellFortune(NumberOfChildren, PartnerName, GeographicLocation, JobTitle){
+    return console.log("You will be a " + JobTitle + " in " + GeographicLocation +
+    " and married to " + PartnerName + " with " + NumberOfChildren + " kids.");
+}
+tellFortune(3, 'Matteo', 'France', 'Android Developer');
+tellFortune(2, 'Victor', 'Brazil', 'Front End Developer');
+tellFortune(5, 'Andrew', 'Berlin', 'Heir');
